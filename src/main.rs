91a2079145571aa0c02e423a1d2cf6319bbda45a
Movitz Sunar <movitz.sunar@gmail.com>
2022-09-29 12:32:56 +0200

    more

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate hw1_chess;
 extern crate piston_window;
 
+use std::f32::consts::PI;
+
 // use adamvib_chess as chess;
-use hw1_chess as chess;
+use hw1_chess::{self as chess, piece};
 use piston_window::{color::hex, *};
 
 const CHESS_SQUARE_LENGTH: u32 = 90;
@@ -22,6 +24,9 @@ fn main() {
 
     let mut mouse_cursor = [0.0, 0.0];
     let mut selected_square = 65;
+    let mut dragged_square = 65;
+    let mut hovered_square = 65;
+
     let mut butt: Option<ButtonArgs> = None;
     while let Some(e) = window.next() {
         let pieces = board.get_pices();
@@ -34,26 +39,33 @@ fn main() {
             if let Button::Mouse(b) = x.button {
                 butt = Some(x);
 
-                if x.state == ButtonState::Press {
-                    let x = (mouse_cursor[0] / (CHESS_SQUARE_LENGTH as f64)).floor() as u32
-                        + 8 * (mouse_cursor[1] / (CHESS_SQUARE_LENGTH as f64)).floor() as u32;
+                hovered_square = (mouse_cursor[0] / (CHESS_SQUARE_LENGTH as f64)).floor() as u32
+                    + 8 * (mouse_cursor[1] / (CHESS_SQUARE_LENGTH as f64)).floor() as u32;
 
-                    let p = pieces[x as usize];
-                    if p.is_some() && p.unwrap().get_color() == board.get_active_color() {
-                        selected_square = x;
-                    } else if selected_square < 64 {
+                if x.state == ButtonState::Release && dragged_square != 65 {
+                    if hovered_square != dragged_square {
                         let res = board.make_move(&hw1_chess::cmove::CMove {
-                            from: selected_square as u8,
-                            to: x as u8,
+                            from: dragged_square as u8,
+                            to: hovered_square as u8,
                             promote_to: hw1_chess::piece::PieceType::Queen,
                         });
 
+                        selected_square = 65;
                         if res.is_err() {
                             println!("make_move error: {}", res.err().unwrap());
                         } else {
                             println!("{}", board.to_fen());
                         }
+                    }
 
+                    dragged_square = 65;
+                }
+                if x.state == ButtonState::Press {
+                    let p = pieces[hovered_square as usize];
+                    if p.is_some() && p.unwrap().get_color() == board.get_active_color() {
+                        dragged_square = hovered_square;
+                        selected_square = hovered_square;
+                    } else if selected_square < 64 {
                         selected_square = 65;
                     }
                 }
@@ -84,8 +96,12 @@ fn main() {
                         c.transform,
                         g,
                     );
-
-                    let piece = pieces[(8 * y + x) as usize];
+                }
+            }
+            for x in 0..8 {
+                for y in 0..8 {
+                    let piece_idx = 8 * y + x;
+                    let piece = pieces[piece_idx as usize];
 
                     if piece.is_some() {
                         let piece = piece.unwrap();
@@ -93,15 +109,27 @@ fn main() {
 
                         let img = &images[idx as usize];
                         let size = img.get_size();
-                        Image::new()
-                            .rect([
-                                ((x as f64 + 0.5) * (CHESS_SQUARE_LENGTH as f64)
-                                    - (size.0 / 2) as f64) as f64,
-                                ((y as f64 + 0.5) * (CHESS_SQUARE_LENGTH as f64)
-                                    - (size.1 / 2) as f64) as f64,
+
+                        let mut coords = [
+                            ((x as f64 + 0.5) * (CHESS_SQUARE_LENGTH as f64) - (size.0 / 2) as f64)
+                                as f64,
+                            ((y as f64 + 0.5) * (CHESS_SQUARE_LENGTH as f64) - (size.1 / 2) as f64)
+                                as f64,
+                            (size.0) as f64,
+                            (size.1) as f64,
+                        ];
+
+                        if piece_idx == dragged_square {
+                            coords = [
+                                mouse_cursor[0] - (size.0 / 2) as f64,
+                                mouse_cursor[1] - (size.1 / 2) as f64,
                                 (size.0) as f64,
                                 (size.1) as f64,
-                            ])
+                            ];
+                        }
+
+                        Image::new()
+                            .rect(coords)
                             .draw(img, &Default::default(), c.transform, g);
                     }
                 }
@@ -114,19 +142,22 @@ fn main() {
                 for mv in moves {
                     let (x, y) = (mv.to % 8, mv.to / 8);
 
-                    let sqr_size = 10.0;
-                    let offset = CHESS_SQUARE_LENGTH as f64 / 2.0 - sqr_size / 2.0;
-                    rectangle(
-                        hex("555555"),
+                    let size = 5.0;
+                    let offset = CHESS_SQUARE_LENGTH as f64 / 2.0 - size / 2.0;
+                    circle_arc(
+                        hex("4a7055"),
+                        size / 2.0,
+                        0.0,
+                        (PI * 2.0).into(),
                         [
                             (CHESS_SQUARE_LENGTH * x as u32) as f64 + offset,
                             (CHESS_SQUARE_LENGTH * y as u32) as f64 + offset,
-                            sqr_size,
-                            sqr_size,
+                            size,
+                            size,
                         ],
                         c.transform,
                         g,
-                    )
+                    );
                 }
             }
         });
